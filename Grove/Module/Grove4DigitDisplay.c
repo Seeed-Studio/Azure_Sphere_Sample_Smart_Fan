96a3f28941dc1c80a69b3fc236ba6cb361599c00
Takashi Matsuoka <matsujirushi@live.jp>
2018-10-12 19:28:15 +0900

    Support over 1sec. at usleep().

--- Grove/Module/Grove4DigitDisplay.c
+++ Grove/Module/Grove4DigitDisplay.c
@@ -26,8 +26,8 @@ static void usleep(long usec)
 	struct timespec req;
 	struct timespec rem;
 
-	req.tv_sec = 0;
-	req.tv_nsec = usec * 1000;
+	req.tv_sec = usec / 1000000;
+	req.tv_nsec = (usec % 1000000) * 1000;
 
 	while (nanosleep(&req, &rem) != 0)
 	{
